5f58f538425d5bb7e6bb5c70fb5aa227bd684fbe
Cairo Stewart <stewartcairo@gmail.com>
2017-03-14 09:25:19 -0400

    Connecting to MySQL DB

---
+++ src/main/java/com/example/LessonController.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/lessons")
+public class LessonController {
+    private final LessonRepository repository;
+
+    public LessonController(LessonRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public Iterable<Lesson> all() {
+        return this.repository.findAll();
+    }
+
+    @PostMapping("")
+    public Lesson create(@RequestBody Lesson lesson) {
+        return this.repository.save(lesson);
+    }
+
+}
---
+++ src/main/java/com/example/LessonRepository.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by localadmin on 3/14/17.
+ */
+public interface LessonRepository extends CrudRepository<Lesson, Long> {
+}
